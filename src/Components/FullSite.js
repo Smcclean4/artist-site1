c753ae5e0b005f2d6552b753fe47940b965da13c
Sidney McClean <sidney.mcclean@outlook.com>
2019-12-23 13:11:34 -0800

    re-creating artist site

---
+++ src/Components/FullSite.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class FullSite extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FullSite
+
