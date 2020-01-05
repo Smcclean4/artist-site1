f935848dd31394cef9ec4442720772fb9c7e52bf
Sidney McClean <sidney.mcclean@outlook.com>
2020-01-05 14:24:32 -0800

    finished nav now working on banner

---
+++ src/Components/Banner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Banner extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Get that money</h1>
+            </div>
+        )
+    }
+}
+
+export default Banner
+
--- src/Components/FullSite.js
+++ src/Components/FullSite.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Navigation } from './Navigation';
+import { Banner } from './Banner';
 
 export class FullSite extends Component {
     render() {
         return (
             <div>
                 <Navigation />
+                <br/>
+                <br/>
+                <Banner />
             </div>
         )
     }
